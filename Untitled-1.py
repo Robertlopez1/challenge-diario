85322474e076df6eff27831a64ef2c4aeb6ace51
Robertlopez1 <ariasrobert143@gmail.com>
2024-06-12 11:22:55 -0400

    Challenge1

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,1 +1,12 @@
-print("Hola")
+
+
+def invertir_cadena_recursiva(cadena):
+    if len(cadena) == 0:
+        return cadena
+    else:
+        return cadena[-1] + invertir_cadena_recursiva(cadena[:-1])
+
+# Solicitar entrada al usuario
+cadena = input("Introduce una cadena de caracteres: ")
+cadena_invertida = invertir_cadena_recursiva(cadena)
+print(f"La cadena invertida es: {cadena_invertida}")
