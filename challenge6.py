27d74ed6e0be8b6d2bbb190747ff69dba8ec225a
Robertlopez1 <ariasrobert143@gmail.com>
2024-06-19 14:54:03 -0400

    Add files via upload
    Conversor de Temperatura: Es un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit.

---
+++ challenge6.py
@@ -0,0 +1,5 @@
+# Conversión de Temperatura: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
+
+celsius = float(input("Grado Celsius: "))
+convertir = 1.8 * celsius + 32
+print(f'{convertir} °F')
